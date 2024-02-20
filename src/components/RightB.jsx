0f22629f8d36f176ee66d8f21d46f2c3d05ae6d6
unknown <olaitan4life>
2024-02-20 15:16:02 +0100

    map Updated

--- src/components/RightB.jsx
+++ src/components/RightB.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import map from "./Access/map.jpg";
 
 const RightB = () => {
   return (
@@ -11,15 +12,29 @@ const RightB = () => {
         <p className="mb-2 font-semibold">
           What new real world thing are you digitizing
         </p>
+        <div className="flex gap-10 font-bold">
+          <button className="p-2 border rounded-full mb-5">Open Space</button>
+          <p className="">In-Space</p>
+        </div>
         <input
           type="text"
           placeholder="Search"
           className="border w-[30rem] p-2 mb-5 rounded-2xl"
         />
+
         <p className="font-bold text-xl"> Most common object</p>
         <button className="border w-50 rounded shadow mt-2 p-2 text-black">
           OpenSpace boundry
         </button>
+        <div>
+          {/* Using map generator :https://google-map-generator.com/ */}
+          <iframe
+            style={{ width: 500, height: 300 }}
+            className="rounded-3xl mt-10"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?width=523&amp;height=403&amp;hl=en&amp;q=5,bola%20street,lagos%20Ilford+(lagos)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
